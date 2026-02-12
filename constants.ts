43df5700a830663bfed765b8b096b5720a9de9d4
alualualuborta-sketch <alualualuborta@gmail.com>
2026-02-12 13:00:47 +0600

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,3 +1,13 @@
+import { Product } from './types';
+
+export const WHATSAPP_NUMBER = "8801877278210";
+export const CURRENCY = "৳";
+
+export const DELIVERY_FEES = {
+  INSIDE_DHAKA: 60,
+  OUTSIDE_DHAKA: 120
+};
+
 export const INITIAL_PRODUCTS: Product[] = [
   {
     id: '1',
@@ -37,64 +47,4 @@ Body: Jorjet fabric with digital print and mirror work
 Salwar: Jorjet fabric with digital print
 Orna: Silk fabric with digital print
 
-Size: 38 - 46
-
-M = 38
-L = 42
-XL = 46`,
-    category: 'Women',
-    images: ['https://i.postimg.cc/4d7zqmJb/1769951921-L-1.jpg'],
-    sizes: ['38', '42', '46'],
-    colors: ['Default'],
-    isBestSeller: false,
-    isNewArrival: true
-  },
-
-  {
-    id: '3',
-    name: 'Stylish Drop Shoulder T-Shirt',
-    price: 550,
-    description: `Fabric: 100% Single Jersey Cotton
-T-Shirt Type: Drop Shoulder
-Design: DTF Print
-Fabrics GSM: 200-220
-Usable for both male and female.
-
-Size: M, L, XL
-
-M = Body 42 | Length 27
-L = Body 44 | Length 28
-XL = Body 46 | Length 29`,
-    category: 'Men',
-    images: ['https://i.postimg.cc/j28tfd7m/1770466119-L-10.jpg'],
-    sizes: ['M', 'L', 'XL'],
-    colors: ['Default'],
-    isBestSeller: false,
-    isNewArrival: true
-  },
-
-  {
-    id: '4',
-    name: 'Stylish Sweatshirt for Winter',
-    price: 550,
-    description: `Product Type: Sweatshirt
-Main Material: Cotton
-Fabrics: Cotton Fleece (Inside Brushed) GSM: 260±
-Sleeve: Full Sleeve
-100% Export Fabric
-Matching Ribbed
-Place Of Origin: Bangladesh
-
-Size: M, L, XL
-
-M - Length 28, Chest 38
-L - Length 29, Chest 40
-XL - Length 30, Chest 42`,
-    category: 'Men',
-    images: ['https://i.postimg.cc/FHyCCCP7/1767253385-L-2.jpg'],
-    sizes: ['M', 'L', 'XL'],
-    colors: ['Default'],
-    isBestSeller: false,
-    isNewArrival: true
-  }
-];
+Size: 38 - 4
