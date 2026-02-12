438c840457434b6968d0a11c850f8aeb4440143f
alualualuborta-sketch <alualualuborta@gmail.com>
2026-02-12 13:05:22 +0600

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,50 +1,49 @@
-import { Product } from './types';
+import { Product } from "./types";
 
 export const WHATSAPP_NUMBER = "8801877278210";
 export const CURRENCY = "৳";
 
 export const DELIVERY_FEES = {
   INSIDE_DHAKA: 60,
-  OUTSIDE_DHAKA: 120
+  OUTSIDE_DHAKA: 120,
 };
 
 export const INITIAL_PRODUCTS: Product[] = [
   {
-    id: '1',
-    name: 'Stylist Casual Polo Shirt For Men',
+    id: "1",
+    name: "Stylist Casual Polo Shirt For Men",
     price: 500,
     description: `Stylist Casual Polo Shirt For Men
 
 Fabric: PK Cotton
-GSM: 210 (±10)
+GSM: 210 (+-10)
 Export Quality
 
 Size: M, L, XL
 
-M Size:
-Length - 27, Chest - 38/39
+M:
+Length - 27
+Chest - 38/39
 
-L Size:
-Length - 28, Chest - 40/41
+L:
+Length - 28
+Chest - 40/41
 
-XL Size:
-Length - 29, Chest - 42/43`,
-    category: 'Men',
-    images: ['https://i.postimg.cc/pVcZ0MRk/1770287132-L-1.jpg'],
-    sizes: ['M', 'L', 'XL'],
-    colors: ['Default'],
+XL:
+Length - 29
+Chest - 42/43`,
+    category: "Men",
+    images: ["https://i.postimg.cc/pVcZ0MRk/1770287132-L-1.jpg"],
+    sizes: ["M", "L", "XL"],
+    colors: ["Default"],
     isBestSeller: false,
-    isNewArrival: true
+    isNewArrival: true,
   },
 
   {
-    id: '2',
-    name: 'Jorjet Digital Print Three Piece',
+    id: "2",
+    name: "Jorjet Digital Print Three Piece",
     price: 1050,
-    description: `Fabric Details:
+    description: `Fabric Details
 
-Body: Jorjet fabric with digital print and mirror work
-Salwar: Jorjet fabric with digital print
-Orna: Silk fabric with digital print
-
-Size: 38 - 4
+B
