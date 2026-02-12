1b0a1d2c8f5527ebb4eee45f259499d45d0a8db3
alualualuborta-sketch <alualualuborta@gmail.com>
2026-02-12 12:49:29 +0600

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,8 +1,19 @@
-{
-  id: '2',
-  name: 'Stylist Casual Polo Shirt For Men',
-  price: 500,
-  description: `Fabric: PK Cotton
+import { Product } from './types';
+
+export const WHATSAPP_NUMBER = "8801877278210";
+export const CURRENCY = "৳";
+
+export const DELIVERY_FEES = {
+  INSIDE_DHAKA: 60,
+  OUTSIDE_DHAKA: 120
+};
+
+export const INITIAL_PRODUCTS: Product[] = [
+  {
+    id: '2',
+    name: 'Stylist Casual Polo Shirt For Men',
+    price: 500,
+    description: `Fabric: PK Cotton
 GSM: 210 (±10)
 Export Quality
 
@@ -10,77 +21,17 @@ Size: M, L, XL
 M - Length: 27, Chest: 38/39
 L - Length: 28, Chest: 40/41
 XL - Length: 29, Chest: 42/43`,
-  category: 'Men',
-  images: ['https://i.postimg.cc/pVcZ0MRk/1770287132-L-1.jpg'],
-  sizes: ['M', 'L', 'XL'],
-  colors: ['Default'],
-  isBestSeller: false,
-  isNewArrival: true
-},
+    category: 'Men',
+    images: ['https://i.postimg.cc/pVcZ0MRk/1770287132-L-1.jpg'],
+    sizes: ['M', 'L', 'XL'],
+    colors: ['Default'],
+    isBestSeller: false,
+    isNewArrival: true
+  },
 
-{
-  id: '3',
-  name: 'Jorjet Digital Print Three Piece',
-  price: 1050,
-  description: `Fabric Details:
-Body: Jorjet fabric with digital print & mirror work
-Seloar: Jorjet fabric digital print
-Orna: Silk fabric with digital print
-
-Sizes Available:
-M = 38
-L = 42
-XL = 46`,
-  category: 'Women',
-  images: ['https://i.postimg.cc/4d7zqmJb/1769951921-L-1.jpg'],
-  sizes: ['38', '42', '46'],
-  colors: ['Digital Print'],
-  isBestSeller: true,
-  isNewArrival: true
-},
-
-{
-  id: '4',
-  name: 'Stylish Drop Shoulder T-Shirt',
-  price: 550,
-  description: `Fabric: 100% Single Jersey Cotton
-T-Shirt Type: Drop Shoulder
-Design: DTF Print
-GSM: 200-220
-Unisex (Male & Female)
-
-Size:
-M = Chest 42 | Length 27
-L = Chest 44 | Length 28
-XL = Chest 46 | Length 29`,
-  category: 'Men',
-  images: ['https://i.postimg.cc/j28tfd7m/1770466119-L-10.jpg'],
-  sizes: ['M', 'L', 'XL'],
-  colors: ['Printed'],
-  isBestSeller: false,
-  isNewArrival: true
-},
-
-{
-  id: '5',
-  name: 'Stylish Sweetshirt for Winter',
-  price: 550,
-  description: `Product Type: Sweatshirt
-Main Material: Cotton Fleece (Inside Brushes)
-GSM: 260±
-Sleeve: Full Sleeve
-100% Export Fabric
-Matching Ribbed
-Origin: Bangladesh
-
-Size:
-M - Length 28 | Chest 38
-L - Length 29 | Chest 40
-XL - Length 30 | Chest 42`,
-  category: 'Men',
-  images: ['https://i.postimg.cc/FHyCCCP7/1767253385-L-2.jpg'],
-  sizes: ['M', 'L', 'XL'],
-  colors: ['Default'],
-  isBestSeller: false,
-  isNewArrival: true
-}
+  {
+    id: '3',
+    name: 'Jorjet Digital Print Three Piece',
+    price: 1050,
+    description: `Fabric Details:
+Body: Jorjet f
